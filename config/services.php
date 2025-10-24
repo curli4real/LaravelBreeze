00764f5b362c6d20496c9ed3e773d7a410ef2cf2
curli4real <pbarnaja@gmail.com>
2025-10-24 20:40:54 +0800

    laravel breeze recaptcha

--- config/services.php
+++ config/services.php
@@ -34,5 +34,10 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
+    
+    'recaptcha' => [
+        'secret' => env('NOCAPTCHA_SECRET'),
+        'sitekey' => env('NOCAPTCHA_SITEKEY'),
+    ],
 
 ];
